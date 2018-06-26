5ee2d038c4716dc4f34b2a566bc5355fd026ebb0
Pedro souza <pedro.souza.psj@gmail.com>
2018-06-26 16:30:21 -0400

    condicionais e laços

---
+++ conditional.py
@@ -0,0 +1,8 @@
+total = 100
+
+if total > 100:
+    print('maior que 100')
+elif total == 100:
+    print('eh igual a 100')
+else : 
+    print('menor que 100')
---
+++ loop.py
@@ -0,0 +1,4 @@
+list = [2,4,6,8,10]
+
+for number in list:
+    print(number)
