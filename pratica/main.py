2f826fe0d91a07b91d05a92bec2f6f661b190460
Pedro souza <pedro.souza.psj@gmail.com>
2018-06-27 16:33:39 -0400

    exemplo de if ternario

--- pratica/main.py
+++ pratica/main.py
@@ -1,27 +1,35 @@
-conta = input('Digite sua conta: ')
-senha = input('Digite sua senha: ')
+import os
 
-contas = {
-    '0001-02':{
-        'senha':'1234',
-        'nome':'Pedro Souza',
-        'valor':10
-    },
-    '0002-03' :{
-        'senha':'5678',
-        'nome':'Souza Pedro',
-        'valor':20
-    }
-}
-
-if conta in contas and senha == contas[conta]['senha']:
+while True:
     print("***************************************************************")
     print("************* Caixa Eletrônico ********************************")
     print("***************************************************************")
-    print("1- saldo")
-    opcao = input("escolha opcao: ")
-    if opcao == '1':
-        print('Bem vindo ' + contas[conta]['nome'])
-        print('Seu saldo eh %s' % contas[conta]['valor'])
-else:
-    print('Conta ou Senha Invalida')
+    conta = input('Digite sua conta: ')
+    senha = input('Digite sua senha: ')
+
+    contas = {
+        '0001-02':{
+            'senha':'1234',
+            'nome':'Pedro Souza',
+            'valor':10
+        },
+        '0002-03' :{
+            'senha':'5678',
+            'nome':'Souza Pedro',
+            'valor':20
+        }
+    }
+
+    if conta in contas and senha == contas[conta]['senha']:
+        print("1- saldo")
+        opcao = input("escolha opcao: ")
+        if opcao == '1':
+            print('Bem vindo ' + contas[conta]['nome'])
+            print('Seu saldo eh %s' % contas[conta]['valor'])
+    else:
+        print('Conta ou Senha Invalida')
+    
+    input('\nPressione <Enter> Para Continuar...')
+    #os.system('cls' if os.nome == 'nt' else 'clear')
+    os.system('cls')
+    os.system('clear')
